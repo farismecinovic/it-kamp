8ae54f1040214ce5aab0936478e7b3e6c8576dea
Faris <faris.mecinovic.np@gmail.com>
2023-03-07 18:58:58 +0100

    start with quiz

--- JS/uvod/app.js
+++ JS/uvod/app.js
@@ -1,73 +1,61 @@
-const usersTable = document.querySelector(".usersTable");
-const submitBtn = document.querySelector("#submit");
+const QUESTIONS = [
+  {
+    id: 1,
+    question: "Koja je najveca planina na svetu?",
+    answers: ["Kilimandzaro", "Kopaonik", "Fudzi", "Mont Everest", "K2"],
+    correct_answer: "Mont Everest",
+    points: 5,
+  },
+  {
+    id: 2,
+    question: "Koliko minuta traje fudbalska utakmica?",
+    answers: [45, 60, 90, 75, 120],
+    correct_answer: 90,
+    points: 5,
+  },
+];
 
-let users = [];
+let CURRENT_POINTS = 0;
 
-const createUserFactory = (name, lastName, occupation) => {
-  return {
-    id: Math.random()
-      .toString(36)
-      .substring(2, 10 + 2),
-    name,
-    lastName,
-    occupation,
-  };
-};
+const maxPoints = QUESTIONS.reduce((prev, curr) => {
+  return prev + curr.points;
+}, 0);
 
-const deleteUser = (userId) => {
-  const newUsers = users.filter((user) => user.id !== userId);
-  users = [...newUsers];
+const initializeQuiz = () => {
+  console.log("Kviz se pokrece..");
 
-  console.log("New Users", users);
-};
+  const questionContainer = document.querySelector("#pitanje");
+  const answersContainer = document.querySelector("#odgovori");
 
-function dugmeFunction() {
-  let userName = document.querySelector("#name").value;
-  let lastName = document.querySelector("#lastName").value;
-  let occupation = document.querySelector("#occupation").value;
+  const question = QUESTIONS[1];
 
-  if (userName !== "" && lastName !== "" && occupation !== "") {
-    const user = createUserFactory(userName, lastName, occupation);
-    users.push(user);
-    renderUsers();
-    console.log("users", users);
+  questionContainer.textContent = question.question;
 
-    document.querySelector("#name").value = "";
-    document.querySelector("#lastName").value = "";
-    document.querySelector("#occupation").value = "";
-  }
-}
+  question.answers.forEach((answer) => {
+    const button = document.createElement("button");
+    button.textContent = answer;
 
-const renderUsers = () => {
-  const currentUser = users[users.length - 1];
-  //Creating new Row and Cells
+    button.onclick = () => {
+      submitAnswer(question.id, button.textContent);
+    };
 
-  const currentRow = document.createElement("tr");
-  const userNameCell = document.createElement("td");
-  const lastNameCell = document.createElement("td");
-  const occupationCell = document.createElement("td");
-  const actionCell = document.createElement("td");
-
-  actionCell.addEventListener("click", () => {
-    usersTable.removeChild(currentRow), deleteUser(currentUser.id);
+    answersContainer.appendChild(button);
   });
-
-  // Appending Cells to created row
-  currentRow.appendChild(userNameCell);
-  currentRow.appendChild(lastNameCell);
-  currentRow.appendChild(occupationCell);
-  currentRow.appendChild(actionCell);
-
-  // Assinging text to created cells.
-  userNameCell.textContent = currentUser.name;
-  lastNameCell.textContent = currentUser.lastName;
-  occupationCell.textContent = currentUser.occupation;
-  actionCell.textContent = "X";
-  // Appending new row to table element in HTML
-  usersTable.appendChild(currentRow);
 };
-//Ocisti input
-//da se ne duplira ovo
-//Stilizovanje
 
-submitBtn.addEventListener("click", dugmeFunction);
+//Kada se funkcija okine, treba da prebaci na sledece pitanje
+//Ukoliko je dati odgovor tacan, dodati korisniku onoliko poena koliko to pitanje nosi,
+//Prebaciti na sledece pitanje, bilo da je odgovor tacan ili netacan
+const submitAnswer = (id, answer) => {
+  const question = QUESTIONS[0];
+
+  if (question.id === id) {
+    //Prebaci na sledece pitanje
+    if (question.correct_answer === answer) {
+      CURRENT_POINTS += question.points;
+      console.log(CURRENT_POINTS);
+    }
+  }
+};
+
+initializeQuiz();
